b66c109a4c8bf8de16dcc49a91b749f9510da5a7
Muhammad Usama <saimali78941@gmail.com>
2020-04-12 04:14:16 +0500

    Updates

---
+++ precache-manifest.f09d49aa3433bd531c2a95d8d00d0d46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef4d2dce79a917afc1b8bacba6d01863",
+    "url": "/React-App/index.html"
+  },
+  {
+    "revision": "62831baf88ed051f72a1",
+    "url": "/React-App/static/css/main.413dd881.chunk.css"
+  },
+  {
+    "revision": "b9d6adacb41070f9589c",
+    "url": "/React-App/static/js/2.e841496f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App/static/js/2.e841496f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62831baf88ed051f72a1",
+    "url": "/React-App/static/js/main.d5463f26.chunk.js"
+  },
+  {
+    "revision": "aafde0b21a1588767121",
+    "url": "/React-App/static/js/runtime-main.813ae60e.js"
+  }
+]);
